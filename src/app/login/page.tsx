29c50568f566b879d635c6548f3d3c039b2984eb
mayjary <mayankpoojary123@gmail.com>
2025-01-12 17:06:56 +0530

    ok

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -32,7 +32,7 @@ export default function LoginPage() {
         <CardFooter className="flex flex-col space-y-4">
           <Button className="w-full">Login</Button>
           <p className="text-sm text-center">
-            Don't have an account?{" "}
+            Dont have an account?{" "}
             <Link href="/signup" className="text-primary hover:underline">
               Sign up
             </Link>
